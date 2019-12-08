365f14d3e0dcf4443378e9ba2e536c6f747715f4
chepaz <chepaz@chepnet.lan>
2019-12-08 21:03:28 +0100

    Read in/out, print, return dict

--- ffpi_sht75_read_sensor.py
+++ ffpi_sht75_read_sensor.py
@@ -7,24 +7,37 @@ from sht_sensor import Sht
 sht_indoor = Sht(11, 2)
 sht_outdoor = Sht(11, 3)
 
-print('Temperature', sht_indoor.read_t())
-print('Relative Humidity', sht_indoor.read_rh())
-
 
 def sht75_read_sensor():
-    # Read the SHT, calc absolute humidity
-    sht_temperature = sht1x.read_temperature_C()
-    sht_humidity = sht1x.read_humidity()
-    sht_dewpoint = sht1x.calculate_dew_point(sht_temperature, sht_humidity)
+    # Read indoor sensor
+    sht_temperature = sht_indoor.read_t()
+    sht_humidity = sht_indoor.read_rh()
+    sht_dewpoint = sht_indoor.read_dew_point()
     sht_absolute = 216.7 * (sht_humidity / 100.0 * 6.112 *
                             math.exp(17.62 * sht_temperature /
-                            (243.12 + sht_temperature)) /
+                                     (243.12 + sht_temperature)) /
                             (273.15 + sht_temperature))
-    # Formatting things
-    hval = format(sht_humidity, "2.1f")
-    tval = format(sht_temperature, "2.1f")
-    dval = format(sht_dewpoint, "2.1f")
-    aval = format(sht_absolute, "2.1f")
 
-    csvList = {"RH": hval, "T": tval, "DP": dval, "AH": aval}
-    return csvList
+    hval_indoor = format(sht_humidity, "2.1f")
+    tval_indoor = format(sht_temperature, "2.1f")
+    dval_indoor = format(sht_dewpoint, "2.1f")
+    aval_indoor = format(sht_absolute, "2.1f")
+
+    # Read outdoor sensor
+    sht_temperature = sht_outdoor.read_t()
+    sht_humidity = sht_outdoor.read_rh()
+    sht_dewpoint = sht_outdoor.read_dew_point()
+    sht_absolute = 216.7 * (sht_humidity / 100.0 * 6.112 *
+                            math.exp(17.62 * sht_temperature /
+                                     (243.12 + sht_temperature)) /
+                            (273.15 + sht_temperature))
+
+    hval_outdoor = format(sht_humidity, "2.1f")
+    tval_outdoor = format(sht_temperature, "2.1f")
+    dval_outdoor = format(sht_dewpoint, "2.1f")
+    aval_outdoor = format(sht_absolute, "2.1f")
+
+    csvlist = {"RHin": hval_indoor, "Tin": tval_indoor, "DPin": dval_indoor, "AHin": aval_indoor, "RHout": hval_outdoor,
+               "Tout": tval_outdoor, "DPout": dval_outdoor, "AHout": aval_outdoor}
+    print(csvlist)
+    return csvlist
